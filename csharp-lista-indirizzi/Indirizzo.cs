8f0a342b33dbfc3e36606c71ff7dcb1c76181a54
UMBY2407 <umby2407@gmail.com>
2023-04-26 15:53:02 +0200

    Added ToString method and exception on zip parameter

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+	public class Indirizzo
+	{
+		private string name;
+		private string surname;
+		private string street;
+		private string city;
+		private string province;
+		private string zip;
+		
+		//COSTRUTTORE
+		public Indirizzo(string nome, string cognome, string strada, string citta, string provincia, string cap)
+		{
+			this.name = nome;
+			this.surname = cognome;
+			this.street = strada;
+			this.city = citta;
+			this.province = provincia;
+			this.zip = cap;
+			if(this.zip.Length > 5)
+			{
+				throw new ArgumentException("Il codice postale supera il numero di cifre!", "cap");
+			}
+		}
+
+		//GETTERS
+
+		public string GetName()
+		{
+			return this.name;
+		}
+
+		public string GetSurname()
+		{
+			return this.surname;
+		}
+
+		public string GetStreet()
+		{
+			return this.street;
+		}
+
+		public string GetCity()
+		{
+			return this.city;
+		}
+
+		public string GetProvince()
+		{
+			return this.province;
+		}
+
+		public string GetZip()
+		{
+			return this.zip;
+		}
+
+		//METODI
+		public override string ToString()
+		{
+			string stringa = $"--------- Indirizzo di {this.name} {this.surname} ---------\n";
+			stringa += $"\tVia: {this.street}\n";
+			stringa += $"\tCittà: {this.city}\n";
+			stringa += $"\tProvincia: {this.province}\n";
+			stringa += $"\tZIP: {this.zip}\n";
+			stringa += "-----------------------------------";
+			return stringa;
+		}
+	}
+}
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp_lista_indirizzi;
+List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
+try
+{
+	StreamReader file = File.OpenText("C:\\Users\\Umberto\\Desktop\\Informatica\\Corso Generation .NET\\esercizi C#\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\Lista\\addresses.csv");
+	int contatoreRigheLette = 0;
+
+	while (!file.EndOfStream)
+	{
+		string rigaIndirizzoLetta = file.ReadLine();
+		contatoreRigheLette++;
+		if(contatoreRigheLette > 1)
+		{
+			string[] informazioniSeparate = rigaIndirizzoLetta.Split(',');
+			if(informazioniSeparate.Length != 6)
+			{
+				Console.WriteLine("La riga " + contatoreRigheLette + " non rispetta il numero delle informazioni richieste per l'indirizzo per riga");
+			}
+			else
+			{
+				try
+				{
+					Indirizzo IndirizzoLetto = new Indirizzo(informazioniSeparate[0], informazioniSeparate[1], informazioniSeparate[2], informazioniSeparate[3], informazioniSeparate[4], informazioniSeparate[5]);
+					listaIndirizzi.Add(IndirizzoLetto);
+				}catch (ArgumentException e)
+				{
+					Console.WriteLine("C'è stato un errore durante la lettura dell'indirizzo, alla riga n°" + contatoreRigheLette);
+					Console.WriteLine(e.Message);
+				}
+			}
+		}
+	}
+}
+catch (Exception e)
+{
+	Console.WriteLine("Qualcosa è andato storto...");
+	Console.WriteLine(e.Message);
+}
+
+foreach(Indirizzo indirizzo in listaIndirizzi)
+{
+	Console.WriteLine(indirizzo);
+}
